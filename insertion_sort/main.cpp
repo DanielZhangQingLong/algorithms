84bd20cea9e7631d2bd8491fa60ea2bda791a26c
daniel <thecndra@gmail.com>
2019-03-16 18:25:44 +0800

    Add insertion sort

---
+++ insertion_sort/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include "SortTestHelper.h"
+
+using namespace std;
+
+template <typename T>
+void insertionSort(T arr[], int n) {
+    for (int i = 1; i < n; i ++) {
+        for (int j = i; j > 0 && arr[j] < arr[j-1]; j --) {
+            swap(arr[j], arr[j - 1]);
+        }
+    }
+}
+
+template<typename T>
+void selectionSort( T arr[], int n) {
+    for (int i = 0; i < n; i ++) {
+        // Find out min in [i, n)
+        int minIndex = i;
+        for (int j = i + 1; j < n; j ++) {
+            if(arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    int n = 10000;
+    int *arr = SortTestHelper::genenrateRandomArray(n, 0, n);
+    int *arr2 = SortTestHelper::copyIntArray(arr, n);
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
+    SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
+    delete[] arr;
+    delete[] arr2;
+    return 0;
+}
